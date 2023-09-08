2afdf446cd87d2300203ac08cb68038bab053390
Cee-Empire <kaycee.louis09@gmail.Com>
2023-09-07 20:05:31 -0700

    Hello, word

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main - A program that uses printf function to print a line
  * Return: 0 (Successful)
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
