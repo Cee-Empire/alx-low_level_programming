acba680365bc0509adfb4e9769207c2a64193f0a
Cee-Empire <kaycee.louis09@gmail.Com>
2023-09-07 19:08:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print a line with puts funciton
+ * Return: 0 (Done)
+*/
+int main(void)
+{
+puts("\""Programming is like building a multilingual puzzle");
+return:(0);
+}
